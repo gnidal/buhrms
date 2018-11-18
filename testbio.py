0af44db748bf2dfd0d94cac4387c5febc4af12b1
gnidal <gnidal@gmail.com>
2018-11-18 12:29:08 +0200

    testbio

---
+++ testbio.py
@@ -0,0 +1,15 @@
+import sys
+import time
+from zklib import zklib, zkconst
+zk = zklib.ZKLib('10.10.10.26', '4370')
+
+res = zk.connect()
+if res == True
+	print zk.enableDevice()
+	print zk.disableDevice()
+	print zk.version()
+	print zk.osversion()
+	print zk.deviceName()
+	#zk.getUser()
+	#zk.enableDevice()
+	#zk.disconnect()
