bd864e591d99a4d3dc5743513d9fe2f9574c2e16
gnidal <gnidal@gmail.com>
2018-11-19 14:55:29 +0200

    f

--- bio.py
+++ bio.py
@@ -5,7 +5,8 @@ import logging
 import binascii
 #import sys
 #from zklib import zklib
-from . import zklib
+import zklib
+#from . import zklib
 from .zkconst import *
 import time
 #from zklib import zkconst
